f44d3f449d3eb7865c89f1ae6afe59811b760012
BH <blakehyland95@gmail.com>
2021-08-28 14:57:05 -0600

    added cuty search functionality. Added Past searches to display on page

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,9 +1,62 @@
-var city = "dallas"
+//MAKES ARRAY FROM LOCAL STORAGE OR IS BLANK
+var searchHistory = JSON.parse(localStorage.getItem('City Name')) || [];
+var historyList = $('#city-list')
 
 var apiKey = "bbe2aa3199841348e66e7179736b0e67"
-var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
 
-fetch(apiURL)
-  .then(response => response.json())
-  .then(data => console.log(data));
+//FILTERS ARRAY FOR UNIQUE VALUES
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+var unique = searchHistory.filter(onlyUnique);
+
+
+//When Search button is clicked
+$("#search-btn").on("click", function () {
+  //grabs city name 
+  if (!$(this).siblings('input').val()) {
+    alert("Please input a city name")
+    return
+  } else {
+    var cityName = $(this).siblings('input').val()
+
+    //Sends fetch to openweather map
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data)
+          });
+          //Saves Seach into Array
+          searchHistory.push(cityName)
+          //Pushes Array into localstorage 
+          saveSearch();
+        } else {
+          //If the city input doesnt exist
+          alert('Error: City not found');
+        }
+      });
+  }
+});
+
+//APPENDS ARRAY UNIQUES INTO BUTTONS TO SHOW SEARCH HISTORY
+var grabHistory = function () {
+
+  for (var i = 0; i < unique.length; i++) {
+    var liEl = document.createElement('li');
+    var buttonEl = document.createElement('button')
+    buttonEl.setAttribute('class', 'btn btn-secondary');
+    buttonEl.textContent = unique[i];
+    liEl.append(buttonEl);
+    historyList.append(liEl);
+  }
+}
+
+//SAVES SEARCHES INTO LOCAL STORAGE
+var saveSearch = function () {
+  localStorage.setItem('City Name', JSON.stringify(searchHistory));
+}
+
+grabHistory()
