fbc6b8110696cbd1f337f351cfccc9b1e2503d8a
Bhyland <blakehyland95@gmail.com>
2021-08-30 11:50:47 -0600

    added current weather stats with UV index

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -3,6 +3,7 @@ var searchHistory = JSON.parse(localStorage.getItem('City Name')) || [];
 var historyList = $('#city-list')
 var forecastEl = $('#5dayforecast')
 var forecastTitle = $('#5dayforecast-title')
+var weatherEl = $('#current-weather')
 
 var apiKey = "bbe2aa3199841348e66e7179736b0e67"
 
@@ -33,6 +34,9 @@ $("#search-btn").on("click", function () {
         if (response.ok) {
           response.json().then(function (data) {
             console.log(data)
+            //Current Forecast 
+            currentWeather(data)
+            //Five day forecast
             FiveDayForecast(data)
           });
           //Saves Seach into Array
@@ -68,12 +72,65 @@ var saveSearch = function () {
   localStorage.setItem('City Name', JSON.stringify(searchHistory));
 }
 
+//DISPLAYS CURRENT WEATHER STATS
+var currentWeather = function (weather) {
+  //clears out WeatherEl Div before adding info
+  weatherEl.empty();
+
+  //adds city title 
+  var cityTitle = document.createElement('h2')
+  cityTitle.textContent = weather.city.name + " (" + moment.unix(weather.list[0].dt).format("MMM D, YYYY") + ') ';
+
+  //adds weather icon
+  var forecastImage = document.createElement('img')
+  forecastImage.setAttribute('src', `https://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`)
+  cityTitle.append(forecastImage)
+
+  weatherEl.append(cityTitle)
+
+  //Adds temperature
+  var forecastTemp = document.createElement('p')
+  forecastTemp.textContent = 'Temp: ' + weather.list[0].main.temp + '°F'
+  weatherEl.append(forecastTemp)
+
+  //Gets Wind for each day
+  var forecastWind = document.createElement('p')
+  forecastWind.textContent = 'Wind: ' + weather.list[0].wind.speed + 'MPH';
+  weatherEl.append(forecastWind)
+
+  //Gets Humidity for each day
+  var forecastHumidity = document.createElement('p')
+  forecastHumidity.textContent = 'Humidity: ' + weather.list[0].main.humidity + '%'
+  weatherEl.append(forecastHumidity)
+
+  //Gets Lat and Long of City 
+  var cityLat = weather.city.coord.lat
+  var cityLon = weather.city.coord.lon
+
+
+  //Makes Fetch call to get UVI and adds to weatherEl
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      var cityUviText = document.createElement('p')
+      var cityUvi = document.createElement('span')
+      cityUvi = data.current.uvi
+      cityUviText.textContent = 'UV Index: ' 
+      cityUviText.append(cityUvi)
+      weatherEl.append(cityUviText)
+
+    });
+
+    
+}
+
 //DISPLAY 5 DAY FORECAST 
 var FiveDayForecast = function (weather) {
- forecastEl.empty();
+  //clears out forecastEl Div before adding info
+  forecastEl.empty();
   document.querySelector('#forecast-title').textContent = "5-Day Forecast:";
 
-  for(i=5; i < weather.list.length; i=i+8){
+  for (i = 5; i < weather.list.length; i = i + 8) {
     var forecastCard = document.createElement('div')
     forecastCard.setAttribute('class', 'card col-md-2 border forecastCard')
 
@@ -84,12 +141,12 @@ var FiveDayForecast = function (weather) {
 
     //Gets Image for each day
     var forecastImage = document.createElement('img')
-    forecastImage.setAttribute('src',`https://openweathermap.org/img/wn/${weather.list[i].weather[0].icon}@2x.png`)
+    forecastImage.setAttribute('src', `https://openweathermap.org/img/wn/${weather.list[i].weather[0].icon}@2x.png`)
     forecastCard.append(forecastImage)
 
     //Gets Temp for each day
     var forecastTemp = document.createElement('p')
-    forecastTemp.textContent = 'Temp: ' + weather.list[i].main.temp +'°F'
+    forecastTemp.textContent = 'Temp: ' + weather.list[i].main.temp + '°F'
     forecastCard.append(forecastTemp)
 
     //Gets Wind for each day
@@ -99,15 +156,15 @@ var FiveDayForecast = function (weather) {
 
     //Gets Humidity for each day
     var forecastHumidity = document.createElement('p')
-    forecastHumidity.textContent = 'Humidity: ' + weather.list[i].main.humidity +' %'
+    forecastHumidity.textContent = 'Humidity: ' + weather.list[i].main.humidity + '%'
     forecastCard.append(forecastHumidity)
 
     //Appends Forecast Card to Forecast Container
     forecastEl.append(forecastCard)
   }
-  
 
-  
+
+
 }
 
 
